5028849bc03976ae5b4201bfb3a228526901a285
2Proton2 <apurva22kalvade@gmail.com>
2023-12-03 21:14:43 +0530

    added a auth middleware and logout route

---
+++ webapp-server/src/middlewares/auth.middleware.js
@@ -0,0 +1,83 @@
+const { findOneUserService } = require('../services/admin.service');
+const { comparePassword } = require('../services/auth.service');
+const jwt = require('jsonwebtoken');
+const private_key = process.env.PRIVATE_KEY;
+
+module.exports.auth_middleware = async (req, res, next) => {
+    try {
+        let userExistence = await findOneUserService('email', req.body.email);
+        if(userExistence){
+            let flag = await comparePassword(req.body.password, userExistence.password);
+            if(flag){
+                //generate the token
+                const genToken = await userExistence.generateAuthToken();
+
+                //set the cookie
+                res.cookie("token", genToken, {
+                    expires: new Date(Date.now() + 2592000000),
+                    httponly: true
+                });
+
+                //pass userDetails to next middleware
+                req.user = userExistence;
+                next();
+            }
+            else {
+                throw new Error(`Failed Authentication`)
+            }
+        }
+        else{
+            throw new Error(`Invalid Credential`);
+        }
+    } catch (error) {
+        res.status(401).json({
+            message: `${error}`
+        })
+    }
+}
+
+module.exports.session_middleware = async (req, res, next) => {
+    try {
+        const headerRawData = req.headers;
+        let rawToken = headerRawData.cookie;
+
+        if(rawToken){
+            const token = rawToken.split('=')[1].trim();
+
+            //is token authenticated with our application
+            const tokenVerification = jwt.verify(token, process.env.PRIVATE_KEY);
+            if(Date.now() > tokenVerification.exp){
+                throw new Error("Session middleware : Authentication Failed");
+            }
+
+            if(tokenVerification){
+                const result = await findOneUserService('_id', tokenVerification.id);
+
+                if(result){
+                    let userTokens = result.tokens;
+
+                    //check token is present or not
+                    let isTokenPresent = userTokens.some((index) => index.token === token);
+
+                    if(!isTokenPresent){
+                        throw new Error(`Session middleware : Authentication Failed`);
+                    }
+                    next();
+                }
+                else{
+                    throw new Error(`Session middleware : Can't find the user`);
+                }
+            }
+            else{
+                throw new Error(`Session middleware : Invalid Credentials`)
+            }
+        }
+        else{
+            throw new Error(``)
+        }
+    } catch (error) {
+        res.status(403).json({
+            message: `${error}`
+        })
+    }    
+}
--- webapp-server/src/routes/admin.route.js
+++ webapp-server/src/routes/admin.route.js
@@ -3,30 +3,36 @@ const Route = express.Router();
 const {adminController} = require('../controllers/admin.controller');
 const { loginValidationRules, loginValidate } = require('../validators/login.validator');
 const { createUserValidationRules, createUservalidate } = require('../validators/user_creation.validator');
+const { auth_middleware, session_middleware } = require('../middlewares/auth.middleware');
 
 /**
  * admin login
  */
-Route.post('/login', loginValidationRules(), loginValidate, adminController.log_user);
+Route.post('/login', loginValidationRules(), loginValidate, auth_middleware, adminController.log_user);
 
 /**
  * creating a new user
  */
-Route.post('/add-user', createUserValidationRules(), createUservalidate, adminController.add_user);
+Route.post('/add-user', createUserValidationRules(), createUservalidate, session_middleware, adminController.add_user);
 
 /**
  * deleting a user
  */
-Route.delete('/delete-user/:id', adminController.del_user);
+Route.delete('/delete-user/:id', session_middleware, adminController.del_user);
 
 /**
  * retreiving all the users
  */
-Route.get('/get-all-users', adminController.get_all_user);
+Route.get('/get-all-users', session_middleware, adminController.get_all_user);
 
 /**
  * updating a user details
  */
-Route.put('/update-user/:id', adminController.update_user);
+Route.put('/update-user/:id', session_middleware, adminController.update_user);
+
+/**
+ * logout
+ */
+Route.post('/logout', session_middleware, adminController.logout_user);
 
 module.exports = Route;
